680b95822a731bf9cd3ddbddeb851b07e0086b5a
davidmshou <david.m.shou@gmail.com>
2015-04-04 09:28:47 -0700

    Add subclass function.

--- students/DavidShoubridge/session05/subclasses.py
+++ students/DavidShoubridge/session05/subclasses.py
@@ -39,3 +39,31 @@ def create_instance_of_subclass_with_init_value_from_superclass():
 
     chihuahua = Dog("Bowser", "Woof", 2, 2)
     print(chihuahua.name)  # Returns error
+
+
+def view_all_attributes():
+    """Is it possible to view all of the attributes of given class?
+    """
+    class Animal(object):
+        species = "Mammal"
+
+        def __init__(self, name, sound):
+            self.name = name
+            self.sound = sound
+
+    class Dog(Animal):
+
+        def __init__(self, ears, eyes):
+            self.ears = ears
+            self.eyes = eyes
+
+    chihuahua = Dog(2, 2)
+
+    attributes = vars(chihuahua)
+    print(attributes)
+
+if __name__ == '__main__':
+    subclass_init()
+    # create_instance_of_subclass_with_init_value_from_superclass()  \
+    # Causes TypeError
+    view_all_attributes()
