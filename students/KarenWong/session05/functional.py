052c91a5b54ce87459bd784edc3d3a7cc5db2d79
Karen Wong <karen.wsywong@gmail.com>
2015-04-05 20:05:00 -0700

    add docstrings

--- students/KarenWong/session05/functional.py
+++ students/KarenWong/session05/functional.py
@@ -1,20 +1,24 @@
-#  Can you put lambda in a tuple?
 t = (lambda x, y: x + y,)
+"""
+Can you put lambda in a tuple?
+"""
 print t[0](3, 4)  # Yes
 
 
-#  Can you use "reduce" on tuples?
-
 reduce(lambda x, y: x + y, t)
-
+"""
+Can you use "reduce" on tuples?
+"""
 t = (2, 4, 6, 8, 10)  # Yes
 
 
-#  Can you use "map" on tuples?
 l = [1, 2, 3, 4, 5]
 
 
 def tuple_map(x):
+    """
+    Can you use "map" on tuples?
+    """
     return x * 2
 
 map(tuple_map, l)  # Yes
