e84d5c2caf0f4ffbb1bb8d308d4d564205b2fce6
Karen Wong <karen.wsywong@gmail.com>
2015-04-06 16:39:38 -0700

    add one function

---
+++ students/KarenWong/session05/classes.py
@@ -0,0 +1,16 @@
+def same_attribute():
+    """
+        If two instances of the class have the same attributes are
+        they the same instance?
+    """
+    class Point(object):
+        x = 1
+        y = 2
+    p = Point()
+    g = Point()
+    print p is g
+    print p.x is g.x   # Just to confirm they have the same attributes
+    print p.y is p.y
+
+
+same_attribute()
