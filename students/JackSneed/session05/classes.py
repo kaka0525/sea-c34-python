77eb352a3c8514e315625599d93c04a233ddb6b9
Jack <sneed.jack@gmail.com>
2015-04-06 17:12:26 -0700

    Add classes.py

---
+++ students/JackSneed/session05/classes.py
@@ -0,0 +1,13 @@
+class Build(object):
+    """Can I build a message with an instance os a class?"""
+
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+p = Build("I think", " I got it!")
+
+if __name__ == '__main__':
+    print(p.x + p.y)
