c138afc365c1cdc1fcbf0bcabfc4ef23e6e24208
Jack <sneed.jack@gmail.com>
2015-04-08 14:12:02 -0700

    Add question 2

--- students/JackSneed/session05/subclasses.py
+++ students/JackSneed/session05/subclasses.py
@@ -18,5 +18,21 @@ b = NewBuild("Jack")
 if __name__ == '__main__':
     print("My name is {} and {}{}.".format(b.name, p.x, p.y))
 
-#Question 2
+# Question 2
+
+"""Can I overwrite parent class objects in a child class"""
+class parent(object):
+
+    home = "two-story"
+    car = "black"
+
+class child(parent):
+
+    car = "blue"
+
+if __name__ == '__main__':
+    print("My parents have a {} home. They drive a {} car and mine is {}"
+           .format(parent.home, parent.car, child.car))
+
+# Question 3
 
