f1b89c2c2db7b73fc57fd76911dfb33d88a4ee98
Karen Wong <karen.wsywong@gmail.com>
2015-04-12 21:07:45 -0700

    add step 4 and step 5

--- students/KarenWong/session06/html_render.py
+++ students/KarenWong/session06/html_render.py
@@ -3,8 +3,9 @@ class Element(object):
     tag = u""
     indent = u"    "
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
         self.content = [str(content)] if content else []
+        self.attributes = kwargs
 
     def append(self, string):
         """Append string to content."""
@@ -13,7 +14,10 @@ class Element(object):
     def render(self, file_out, ind=""):
         """Render the tag and strings in content."""
         file_out.write(ind)
-        file_out.write("<{}>\n".format(self.tag))
+        file_out.write("<{}".format(self.tag))
+        for key, value in self.attributes.iteritems():
+            file_out.write(" {key}=\"{value}\"".format(key=key, value=value))
+        file_out.write(">\n")
         for item in self.content:
             if isinstance(item, Element):
                 item.render(file_out, ind + self.indent)
@@ -58,6 +62,25 @@ class OneLineTag(Element):
             else:
                 file_out.write(item)
         file_out.write("</{}>\n".format(self.tag))
+
+
+class SelfClosingTag(Element):
+    """SelfClosingTag"""
+
+    def render(self, file_out, ind=""):
+        file_out.write(ind)
+        file_out.write("<{}".format(self.tag))
+        for key, value in self.attributes.iteritems():
+            file_out.write(" {key}=\"{value}\"".format(key=key, value=value))
+        file_out.write(" />\n")
+
+
+class Hr(SelfClosingTag):
+    tag = u"hr"
+
+
+class Br(SelfClosingTag):
+    tag = u"br"
 
 
 class Title(OneLineTag):
