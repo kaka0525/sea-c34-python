7d3289bee14001e94e3f5ca88e707435c4cc64e3
Karen Wong <karen.wsywong@gmail.com>
2015-04-12 19:15:02 -0700

    add step 3

--- students/KarenWong/session06/html_render.py
+++ students/KarenWong/session06/html_render.py
@@ -14,24 +14,57 @@ class Element(object):
         """Render the tag and strings in content."""
         file_out.write(ind)
         file_out.write("<{}>\n".format(self.tag))
-        for string in self.content:
-            file_out.write(ind)
-            file_out.write(self.indent)
-            file_out.write(string)
-            file_out.write("\n")
+        for item in self.content:
+            if isinstance(item, Element):
+                item.render(file_out, ind + self.indent)
+            else:
+                file_out.write(ind)
+                file_out.write(self.indent)
+                file_out.write(item)
+                file_out.write("\n")
         file_out.write(ind)
         file_out.write("</{}>\n".format(self.tag))
 
 
 class Html(Element):
     """HTML tag."""
-
     tag = u"html"
+
+    def render(self, file_out, ind=""):
+        file_out.write(ind)
+        file_out.write("<!DOCTYPE html>\n")
+        Element.render(self, file_out, ind)
 
 
 class Body(Element):
     """Body tag."""
     tag = u"body"
 
-    def __init__(self):
-        pass
+
+class Head(Element):
+    """Head tag."""
+    tag = u"head"
+
+
+class OneLineTag(Element):
+    """OneLineTag."""
+
+    def render(self, file_out, ind=""):
+        file_out.write(ind)
+        file_out.write("<{}>".format(self.tag))
+        for item in self.content:
+            if isinstance(item, Element):
+                item.render(file_out, ind + self.indent)
+            else:
+                file_out.write(item)
+        file_out.write("</{}>\n".format(self.tag))
+
+
+class Title(OneLineTag):
+    """Title tag."""
+    tag = u"title"
+
+
+class P(Element):
+    """P tag."""
+    tag = u"p"
