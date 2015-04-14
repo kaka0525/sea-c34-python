8e5c6a7d2cedc40f1a38931eefcf88cfb1256cca
Karen Wong <karen.wsywong@gmail.com>
2015-04-14 16:32:27 -0700

    add test_lambda.py

---
+++ students/KarenWong/session07/test_lambda.py
@@ -0,0 +1,11 @@
+import unittest
+from lamb_da import function_builder
+
+
+class LambdatestingCase(unittest.TestCase):
+    def function_builder(self):
+        assert the_list[0](2) == 2
+        assert the_list[1](2) == 3
+
+if __name__ == '__main__':
+    unittest.main()
