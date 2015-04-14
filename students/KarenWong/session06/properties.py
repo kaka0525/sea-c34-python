89f44b1b1a56315470f602be5c2a72be9a1125a5
Karen Wong <karen.wsywong@gmail.com>
2015-04-13 20:09:53 -0700

    add properties file

---
+++ students/KarenWong/session06/properties.py
@@ -0,0 +1,28 @@
+def squarepants():
+    """
+    My problem is to how to use decorators to simplify a function?
+    """
+
+    class Square(object):
+
+        def __init__(self, length=6):
+            self.length = length
+            self._area = length * length
+
+        @property
+        def area(self):
+            return self._area
+
+        @area.setter
+        def area(self, value):
+            self.length = value / value
+            self._area = value
+
+    spongebob = Square()
+    print spongebob.length
+    spongebob.length = 5
+    print spongebob.length
+    print spongebob.area
+
+
+squarepants()
