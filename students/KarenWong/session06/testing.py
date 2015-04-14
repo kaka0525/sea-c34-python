8e1b549056b64ca132776aaa09618a171e870104
Karen Wong <karen.wsywong@gmail.com>
2015-04-13 20:30:02 -0700

    add testing.py

---
+++ students/KarenWong/session06/testing.py
@@ -0,0 +1,8 @@
+def my_division_function(val1, val2):
+    """Can I ignore a zero division error and what's the result in the testing
+    module?
+    """
+    return val1 / val2
+
+if __name__ == '__main__':
+    unittest.main()
