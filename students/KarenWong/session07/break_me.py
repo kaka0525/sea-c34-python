915cb0224f7b939c9f19e377295132fc6db44966
Karen Wong <karen.wsywong@gmail.com>
2015-04-14 22:54:48 -0700

    add break_me.py

---
+++ students/KarenWong/session07/break_me.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+def name_error_function():
+    hello()
+
+
+# def type_error_function():
+#     x = 1
+#     x()
+
+
+# def syntax_error_function():
+#     print("Karen"
+
+
+# def attribute_error_function():
+#     "hello".not_a_function()
+
+
+if __name__ == '__main__':
+    unittest.main()
